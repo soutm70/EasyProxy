e224d21ee1f666d1ef4896c8da22de0e1b60cc37
soutm70 <58353071+soutm70@users.noreply.github.com>
2026-07-31 12:53:29 +1000

    Update dlstreams.py

--- extractors/dlstreams.py
+++ extractors/dlstreams.py
@@ -167,7 +167,7 @@ class DLStreamsExtractor:
                 headers = {
                     "User-Agent": self.base_headers["User-Agent"],
                     "Referer": url,headers = {
-                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
                     "Accept-Language": "en-US,en;q=0.9",
                     "Sec-Fetch-Dest": "iframe",
                     "Sec-Fetch-Mode": "navigate",
